b1987a4b5d4e44f4b6f1946c4df2b8e734d3ec4a
PiyushHMehta <jainpiyush1450@gmail.com>
2024-06-24 21:08:31 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='max-w-6xl mx-auto p-3 space-y-4'>
+            <h1 className='text-2xl font-medium text-amber-500'>About</h1>
+            <p>
+                Welcome to our IMDB clone, your ultimate destination for all things movies and television! Our platform is designed to provide you with comprehensive information about films, TV shows, actors, and industry professionals. From the latest blockbusters to timeless classics, we strive to offer detailed insights, reviews, ratings, and trailers to enhance your viewing experience.
+            </p>
+            <p>
+                Welcome to our IMDB clone, your ultimate destination for all things movies and television! Our platform is designed to provide you with comprehensive information about films, TV shows, actors, and industry professionals. From the latest blockbusters to timeless classics, we strive to offer detailed insights, reviews, ratings, and trailers to enhance your viewing experience. Whether you are a casual viewer looking for your next binge-worthy series or a film aficionado seeking in-depth analysis and trivia, our database has something for everyone. Dive into our user-friendly interface, explore our extensive library, and join a community of fellow entertainment enthusiasts who share your passion for cinema and television.
+            </p>
+            <p>
+                Whether you are a casual viewer looking for your next binge-worthy series or a film aficionado seeking in-depth analysis and trivia, our database has something for everyone. Dive into our user-friendly interface, explore our extensive library, and join a community of fellow entertainment enthusiasts who share your passion for cinema and television.
+            </p>
+        </div>
+    )
+}
